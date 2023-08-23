ed0f9347c25295e5a1b7ab34a6c897f5d022db7d
Hoang Long Nguyen <nhlbo@users.noreply.github.com>
2023-08-23 18:42:27 +0700

    chore: add zsh-vi-mode

--- setup.sh
+++ setup.sh
@@ -12,6 +12,7 @@ git config --global user.name "Hoang Long Nguyen"
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" <<< "exit"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
 
 # Copy ZSH file
 cp .zshrc $HOME

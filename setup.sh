4e1dbc430713a290411d940e5705cae85027caf6
Hoang Long Nguyen <nhlbo@users.noreply.github.com>
2023-08-30 18:16:27 +0700

    remove before installing

--- setup.sh
+++ setup.sh
@@ -9,23 +9,26 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 git config --global user.email "nhlbo@users.noreply.github.com"
 git config --global user.name "Hoang Long Nguyen"
 
-# Install oh-my-zsh
+# Install oh-my-zsh and plugins
+rm -rf ${ZSH}
+ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" <<< "exit"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
+git clone https://github.com/jeffreytse/zsh-vi-mode ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode
 
 # Copy ZSH file
 cp .zshrc $HOME
 
 # Copy fonts
-cp -a fonts/SF-Mono-Nerd-Font/*.otf ~/Library/Fonts/
+cp -a fonts/SFMono-Nerd-Font-Ligaturized/*.otf ~/Library/Fonts/
 
 # Install tmux
 brew install tmux
 cp .tmux.conf $HOME
 
 # Install NvChad
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+rm -rf ~/.config/nvim/
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 
 # Copy neovim config
-cp custom/ ~/.config/nvim/lua/custom/
+cp -r -f custom/ ~/.config/nvim/lua/custom/

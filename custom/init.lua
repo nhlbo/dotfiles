f811d17379cae5c9fa233d7e9db5d34f0ea6e573
Hoang Long Nguyen <nhlbo@users.noreply.github.com>
2023-09-03 23:32:00 +0700

    check buffer valid before set buffer

--- custom/init.lua
+++ custom/init.lua
@@ -14,7 +14,10 @@ vim.wo.relativenumber = true
 vim.api.nvim_set_keymap("v", "<M-c>", '"+y', { noremap = true })
 for i = 1, 9, 1 do
     vim.keymap.set("n", string.format("<M-%s>", i), function()
-      vim.api.nvim_set_current_buf(vim.t.bufs[i])
+        local buf = vim.t.bufs[i]
+        if buf and vim.api.nvim_buf_is_valid(buf) then
+            vim.api.nvim_set_current_buf(buf)
+        end
     end)
 end
 for i = 1, 9, 1 do

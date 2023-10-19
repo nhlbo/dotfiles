0d960f7d31569761a8b24746eb1ea898489b0a47
Hoang Long Nguyen <nhlbo@users.noreply.github.com>
2023-10-19 20:32:32 +0700

    feat: format on save

--- custom/configs/null-ls.lua
+++ custom/configs/null-ls.lua
@@ -1,21 +1,37 @@
-local null_ls = require "null-ls"
+local null_ls = require("null-ls")
 
 local b = null_ls.builtins
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 local sources = {
 
-  -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+	-- webdev stuff
+	b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+	b.formatting.prettier.with({ filetypes = { "html", "markdown", "css" } }), -- so prettier works only on these filetypes
 
-  -- Lua
-  b.formatting.stylua,
+	-- Lua
+	b.formatting.stylua,
 
-  -- cpp
-  b.formatting.clang_format,
+	-- cpp
+	b.formatting.clang_format,
 }
 
-null_ls.setup {
-  debug = true,
-  sources = sources,
-}
+null_ls.setup({
+	debug = true,
+	sources = sources,
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) nstead
+					vim.lsp.buf.format({ async = true })
+				end,
+			})
+		end
+	end,
+})
--- custom/init.lua
+++ custom/init.lua
@@ -7,13 +7,13 @@
 -- })
 
 vim.o.autoread = true
-vim.cmd[[
+vim.cmd([[
   autocmd CursorHold * checktime
-]]
+]])
 vim.wo.relativenumber = true
 vim.api.nvim_set_keymap("v", "<c-c>", "y", { silent = true })
 vim.api.nvim_set_keymap("n", "<bs>", "hx", { silent = true })
-vim.api.nvim_set_keymap("v", "<bs>", "\"_d", { silent = true })
+vim.api.nvim_set_keymap("v", "<bs>", '"_d', { silent = true })
 vim.api.nvim_set_keymap("n", "<c-s>", ":w<cr>", { silent = true })
 -- vim.api.nvim_set_keymap("n", "<Tab>", ">>", { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap("n", "<S-Tab>", "<<", { noremap = true, silent = true })
@@ -21,10 +21,10 @@ vim.api.nvim_set_keymap("x", "<tab>", ">gv", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("x", "<s-tab>", "<gv", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("i", "<s-tab>", "<c-d>", { noremap = true, silent = true })
 for i = 1, 9, 1 do
-  vim.keymap.set("n", string.format("<c-t>%s", i), function()
-      local buf = vim.t.bufs[i]
-      if buf and vim.api.nvim_buf_is_valid(buf) then
-          vim.api.nvim_set_current_buf(buf)
-      end
-  end)
+	vim.keymap.set("n", string.format("<c-t>%s", i), function()
+		local buf = vim.t.bufs[i]
+		if buf and vim.api.nvim_buf_is_valid(buf) then
+			vim.api.nvim_set_current_buf(buf)
+		end
+	end)
 end
